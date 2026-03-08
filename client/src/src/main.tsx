db585804c891cb5159bbd633f77851c3c146cff0
souf199643-hash <souf199643@gmail.com>
2026-03-08 13:33:12 +0100

    Update main.tsx

--- client/src/src/main.tsx
+++ client/src/src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dashboard from './pages/Dashboard';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+);
